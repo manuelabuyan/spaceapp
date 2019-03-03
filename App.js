345c1e87d646234581c9b980970cfec0fcc50814
manuelabuyan <manuel.abuyan@gmail.com>
2019-03-03 22:24:40 +1300

    Router set up

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+
+import { AppNavigator } from './config/router';
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! hello??</Text>
-      </View>
+      <AppContainer />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ config/router.js
@@ -0,0 +1,89 @@
+import { createStackNavigator } from 'react-navigation';
+
+import Home from '../screens/Home';
+
+export const AppNavigator = createStackNavigator (
+    {
+    Home: {
+      screen: Home
+    }
+    },{
+        initialRouteName: "Home",
+        headerMode: 'none',
+        transitionConfig: () => ({
+            transitionSpec: {
+            duration: 800
+            },
+            screenInterpolator: (props) => {
+            return transitionConfiguration(props)
+            }
+        })
+    }
+);
+
+  
+//   }, {
+//     headerMode: 'none',
+//     initialRouteName: 'home',
+//     transitionConfig: () => ({
+//       transitionSpec: {
+//         duration: 800
+//       },
+//       screenInterpolator: (props) => {
+//         return transitionConfiguration(props)
+//       }
+//     })
+//   })
+  
+  
+const transitionConfiguration = (props) => {
+    const { layout, position, scene } = props
+    const { index, route } = scene;
+    const params = route.params || {};
+    const transition = params.transition || 'other';
+
+    return {
+        fade: FadeTransition(index, position, layout),
+        fadePause: FadePauseTransition(index, position, layout),
+        default: DefaultTransition(index, position, layout),
+    }[transition]
+}
+  
+const DefaultTransition = (index, position, layout) => {
+    const width = layout.initWidth
+
+    const translateY = 0;
+    const translateX = position.interpolate({
+        inputRange: [index - 1, index],
+        outputRange: [width+width, 0],
+    });
+    return {
+        transform: [
+            { translateX },
+            { translateY }
+        ],
+    }
+}
+
+const FadeTransition = (index, position, layout) => {
+    const opacity = position.interpolate({
+    inputRange: [index - 1, index],
+    outputRange: [0, 1],
+    })
+
+    return {
+        opacity
+    }
+}
+
+const FadePauseTransition = (index, position, layout) => {
+    const opacity = position.interpolate({
+    inputRange: [index - 1, index],
+    outputRange: [0, 0.8],
+    })
+
+    return {
+        opacity
+    }
+}
+
